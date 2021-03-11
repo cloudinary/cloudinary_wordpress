e1590702ee4b8fb38aa7c3dfc1ca8a3443322ed4
Marco Pereirinha <marco.pereirinha@xwp.co>
2021-03-11 09:46:15 +0000

    Update compiled assets

--- js/gallery-block.asset.php
+++ js/gallery-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e417123675a0b2c27eff9ad110a1ef44');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '95635dcdce3468c638dab46e572af7a3');
