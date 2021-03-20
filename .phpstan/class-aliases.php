75be960829f3f6fbce2aff72d78715d96ede7fcb
Marco Pereirinha <marco.pereirinha@xwp.co>
2021-03-20 16:09:00 +0000

    Cleanup

--- .phpstan/class-aliases.php
+++ .phpstan/class-aliases.php
@@ -2,12 +2,3 @@
 
 class WPCOM_VIP_CLI_Command {}
 class WP_CLI_Command {}
-class WP_CLI {
-	public function stop_the_insanity() {}
-	public static function add_command() {}
-	public static function colorize() {}
-	public static function line() {}
-	public static function log() {}
-	public static function warning() {}
-}
-
