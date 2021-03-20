2c3d6261cf692c65547d37d52e7c5fd3328f7753
Marco Pereirinha <marco.pereirinha@xwp.co>
2021-03-20 15:50:13 +0000

    Add phpstan stubs

---
+++ .phpstan/class-aliases.php
@@ -0,0 +1,13 @@
+<?php
+
+class WPCOM_VIP_CLI_Command {}
+class WP_CLI_Command {}
+class WP_CLI {
+	public function stop_the_insanity() {}
+	public static function add_command() {}
+	public static function colorize() {}
+	public static function line() {}
+	public static function log() {}
+	public static function warning() {}
+}
+
---
+++ .phpstan/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+define( 'WPCOM_IS_VIP_ENV', '' );
+define( 'WPINC', '' );
+define( 'LOGGED_IN_COOKIE', '' );
+define( 'CLDN_URL', '' );
+define( 'CLDN_PATH', '' );
