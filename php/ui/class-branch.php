7d29848ea7d02aaf9d87f9692130cc81e60b0ad6
David Cramer <david@digilab.co.za>
2021-03-22 14:17:00 +0200

    add new components

---
+++ php/ui/class-branch.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * Branch UI Component.
+ *
+ * @package Cloudinary
+ */
+
+namespace Cloudinary\UI;
+
+use Cloudinary\UI;
+
+/**
+ *  Component.
+ *
+ * @package Cloudinary\UI
+ */
+class Branch {
+
+	/**
+	 * Holds all the paths.
+	 *
+	 * @var array
+	 */
+	public $paths = array();
+
+	/**
+	 * Holds the name.
+	 *
+	 * @var string|null
+	 */
+	public $name = null;
+
+	/**
+	 * Holds the full path.
+	 *
+	 * @var string
+	 */
+	public $value = '';
+
+	/**
+	 * Holds the unique ID
+	 *
+	 * @var string
+	 */
+	public $id = null;
+
+	/**
+	 * Holds if the value is checked.
+	 *
+	 * @var bool
+	 */
+	public $checked = false;
+
+	/**
+	 * Holds the list of the masters.
+	 *
+	 * @var array
+	 */
+	public $handlers = array();
+
+	/**
+	 * Render component for a setting.
+	 * Component constructor.
+	 *
+	 * @param string $name The name for this branch.
+	 */
+	public function __construct( $name ) {
+		$this->name = basename( $name );
+		$this->id   = $name;
+	}
+
+	/**
+	 * Get the path part.
+	 *
+	 * @param string $part Part to try get.
+	 *
+	 * @return Branch
+	 */
+	public function get_path( $part ) {
+		if ( ! isset( $this->paths[ $part ] ) ) {
+			$this->paths[ $part ]           = new Branch( $this->id . '/' . $part );
+			$this->paths[ $part ]->handlers = $this->handlers;
+		}
+
+		return $this->paths[ $part ];
+	}
+
+	/**
+	 * Get the toggle part.
+	 *
+	 * @return array
+	 */
+	public function get_toggle() {
+		$struct                        = array();
+		$struct['element']             = 'label';
+		$struct['attributes']['class'] = array(
+			'cld-input-on-off-control',
+			'mini',
+		);
+		$struct['children']['input']   = $this->input();
+		$struct['children']['slider']  = $this->slider();
+
+		return $struct;
+	}
+
+	/**
+	 * Filter the name parts structure.
+	 *
+	 * @return array
+	 */
+	protected function get_name() {
+		$struct                          = array();
+		$struct['element']               = 'label';
+		$struct['attributes']['class'][] = 'description';
+		$struct['attributes']['for']     = $this->id;
+		$struct['content']               = $this->name;
+
+		return $struct;
+	}
+
+	/**
+	 * Filter the slider parts structure.
+	 *
+	 * @return array
+	 */
+	public function slider() {
+		$struct                        = array();
+		$struct['element']             = 'span';
+		$struct['render']              = true;
+		$struct['attributes']['class'] = array(
+			'cld-input-on-off-control-slider',
+		);
+		$struct['attributes']['style'] = array();
+
+		return $struct;
+	}
+
+	/**
+	 * Filter the input parts structure.
+	 *
+	 * @return array
+	 */
+	public function input() {
+		$struct                          = array();
+		$struct['element']               = 'input';
+		$struct['attributes']['id']      = $this->id;
+		$struct['attributes']['type']    = 'checkbox';
+		$struct['attributes']['value']   = $this->value;
+		$struct['attributes']['checked'] = 'checked';
+		$struct['attributes']['class']   = array(
+			'cld-ui-input',
+		);
+		$struct['render']                = true;
+		if ( ! empty( $this->paths ) ) {
+			$struct['attributes']['data-master'] = wp_json_encode( $this->get_ids() );
+		}
+
+		return $struct;
+	}
+
+	/**
+	 * Render the parts together.
+	 *
+	 * @return array
+	 */
+	public function render() {
+
+		$children = array();
+		foreach ( $this->paths as $key => $branch ) {
+			$children[ $branch->id ] = $branch->render();
+		}
+
+		$struct = array(
+			'element'    => 'li',
+			'id'         => $this->id,
+			'name'       => $this->name,
+			'value'      => $this->value,
+			'checked'    => false,
+			'master'     => $this->get_ids(),
+			'attributes' => array(
+				'class' => array(
+					'tree-trunk',
+				),
+			),
+			'children'   => array(
+				'toggle' => $this->get_toggle(),
+				'name'   => $this->get_name(),
+			),
+		);
+
+		if ( ! empty( $children ) ) {
+			$struct['children']['branches'] = array(
+				'element'    => 'ul',
+				'attributes' => array(
+					'class' => array(
+						'tree-branch',
+					),
+				),
+				'children'   => $children,
+			);
+		}
+
+		return $struct;
+	}
+
+	/**
+	 * Get the IDS used under this.
+	 *
+	 * @return array
+	 */
+	public function get_ids() {
+		$ids = array();
+		foreach ( $this->paths as $key => $branch ) {
+			$ids[] = $branch->id;
+		}
+
+		return $ids;
+	}
+
+}
--- php/ui/component/class-on-off.php
+++ php/ui/component/class-on-off.php
@@ -19,7 +19,7 @@ class On_Off extends Text {
 	 *
 	 * @var string
 	 */
-	protected $blueprint = 'label|title|tooltip/|/title|prefix/|/label|wrap|control|false_value/|input/|slider/|/control|description/|/wrap';
+	protected $blueprint = 'label|title|tooltip/|/title|prefix/|/label|wrap|description_left/|control|false_value/|input/|slider/|/control|description/|/wrap';
 
 	/**
 	 * Filter the false_value parts structure.
@@ -42,6 +42,25 @@ class On_Off extends Text {
 	}
 
 	/**
+	 * Sets the left hand description.
+	 *
+	 * @param array $struct The array structure.
+	 *
+	 * @return array|null
+	 */
+	protected function description_left( $struct ) {
+		if ( $this->setting->has_param( 'description_left' ) ) {
+			$struct                          = $this->description( $struct );
+			$struct['attributes']['class'][] = 'left';
+			$struct['content']               = $this->setting->get_param( 'description_left' );
+
+			return $struct;
+		}
+
+		return null;
+	}
+
+	/**
 	 * Filter the input parts structure.
 	 *
 	 * @param array $struct The array structure.
@@ -50,16 +69,26 @@ class On_Off extends Text {
 	 */
 	protected function input( $struct ) {
 
-		$struct['element']             = 'input';
-		$struct['attributes']['type']  = 'checkbox';
-		$struct['attributes']['name']  = $this->get_name();
-		$struct['attributes']['id']    = $this->setting->get_slug();
-		$struct['attributes']['value'] = 'on';
+		$struct['element']                       = 'input';
+		$struct['attributes']['type']            = 'checkbox';
+		$struct['attributes']['name']            = $this->get_name();
+		$struct['attributes']['id']              = $this->setting->get_slug();
+		$struct['attributes']['value']           = 'on';
+		$struct['attributes']['data-controller'] = $this->setting->get_slug();
 		if ( 'on' === $this->setting->get_value() ) {
 			$struct['attributes']['checked'] = 'checked';
 		}
-		$struct['attributes']['class'] = 'cld-ui-input';
+		$struct['attributes']['class'] = array(
+			'cld-ui-input',
+		);
 		$struct['render']              = true;
+		if ( $this->setting->has_param( 'master' ) ) {
+			$struct['attributes']['data-master'] = wp_json_encode( $this->setting->get_param( 'master' ) );
+		}
+
+		if ( true === $this->setting->has_param( 'master_required', false ) && empty( $struct['attributes']['data-master'] ) ) {
+			$struct['attributes']['disabled'] = 'disabled';
+		}
 
 		return $struct;
 	}
@@ -77,6 +106,12 @@ class On_Off extends Text {
 		$struct['attributes']['class'] = array(
 			'cld-input-' . $this->type . '-control',
 		);
+		if ( true === $this->setting->get_param( 'mini', false ) ) {
+			$struct['attributes']['class'][] = 'mini';
+		}
+		if ( true === $this->setting->get_param( 'master_required', false ) && empty( $this->setting->get_param( 'master', array() ) ) ) {
+			$struct['attributes']['class'][] = 'disabled';
+		}
 
 		return $struct;
 	}
@@ -94,6 +129,28 @@ class On_Off extends Text {
 		$struct['attributes']['class'] = array(
 			'cld-input-' . $this->type . '-control-slider',
 		);
+		$struct['attributes']['style'] = array();
+		if ( $this->setting->has_param( 'disabled_color' ) ) {
+			$struct['attributes']['style'][] = 'background-color:' . $this->setting->get_param( 'disabled_color' ) . ';';
+		}
+
+		$on                         = $this->get_part( 'i' );
+		$on['attributes']['class']  = array(
+			'icon-on',
+			'dashicons',
+			$this->setting->get_param( 'on' ),
+		);
+		$on['render']               = true;
+		$off                        = $this->get_part( 'i' );
+		$off['attributes']['class'] = array(
+			'icon-off',
+			'dashicons',
+			$this->setting->get_param( 'off' ),
+		);
+		$off['render']              = true;
+
+		$struct['children']['on']  = $on;
+		$struct['children']['off'] = $off;
 
 		return $struct;
 	}
