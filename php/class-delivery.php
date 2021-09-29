08b65e747d51e771aff9866f533019ddf4af3be4
David Cramer <david@digilab.co.za>
2021-09-29 10:40:13 +0200

    fix formatting

--- php/class-delivery.php
+++ php/class-delivery.php
@@ -433,7 +433,7 @@ class Delivery implements Setup {
 			$tag_element['atts']['data-optsize']   = size_format( $remote_size );
 			$tag_element['atts']['data-optformat'] = get_post_meta( $tag_element['id'], Sync::META_KEYS['remote_format'], true );
 			if ( ! empty( $local_size ) && ! empty( $remote_size ) ) {
-				$diff = $local_size - $remote_size;
+				$diff                                = $local_size - $remote_size;
 				$tag_element['atts']['data-percent'] = round( $diff / $local_size * 100, 1 );
 			}
 			$tag_element['atts']['data-permalink'] = get_edit_post_link( $tag_element['id'] );
