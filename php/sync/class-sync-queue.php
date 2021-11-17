21e0833e054c136a750451b3f1a3fdf714f192c3
David Cramer <david@digilab.co.za>
2021-11-17 17:04:09 +0200

    ensure it doesn't go negitive

--- php/sync/class-sync-queue.php
+++ php/sync/class-sync-queue.php
@@ -364,6 +364,11 @@ class Sync_Queue {
 		$total_queued      = $data['total_queued'];
 		$errors_count      = $data['errors'];
 
+		// Prevent negative numbers.
+		if ( 0 > $total_unoptimized ) {
+			$total_unoptimized = 0;
+		}
+
 		$unoptimized_text = __( 'All assets optimized.', 'cloudinary' );
 		if ( 0 < $total_queued && 0 === $total_unoptimized ) {
 			$unoptimized_text = __( 'Optimizing assets.', 'cloudinary' );
