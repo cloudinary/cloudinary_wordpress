c32315f18465bcf858a56cd6e7173c5c460fab24
Marco Pereirinha <marco.pereirinha@xwp.co>
2022-02-17 10:38:57 +0000

    Update compiled assets

--- js/inline-loader.asset.php
+++ js/inline-loader.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '34ad4f649f3b38980c33851cc4b0cf7b');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'ff70d454355a0522afc3a8ec5930a899');
