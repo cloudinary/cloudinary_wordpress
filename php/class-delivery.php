0072e0884306720cc57aef604c35be7d677c3dcd
Marco Pereirinha <marco.pereirinha@xwp.co>
2023-01-09 17:40:36 +0000

    Get a keyless array when getting the size from URL

--- php/class-delivery.php
+++ php/class-delivery.php
@@ -269,7 +269,7 @@ class Delivery implements Setup {
 			$size            = $this->media->get_size_from_url( $original_url );
 			$transformations = $this->media->get_transformations_from_string( $original_url );
 			if ( 'image' === $this->media->get_resource_type( $result['post_id'] ) ) {
-				$attachment_url = wp_get_attachment_image_url( $result['post_id'], array_values( $size ) );
+				$attachment_url = wp_get_attachment_image_url( $result['post_id'], $size );
 			} else {
 				$attachment_url = wp_get_attachment_url( $result['post_id'] );
 			}
@@ -1394,8 +1394,8 @@ class Delivery implements Setup {
 	/**
 	 * Get the registered size.
 	 *
-	 * @param int   $attachment_id The attachment ID.
-	 * @param array $size          The size array.
+	 * @param int         $attachment_id The attachment ID.
+	 * @param false|array $size          The size array or false.
 	 *
 	 * @return string
 	 */
