e9ae9996152f4141d8e40dde627c122042dd1c1c
Marco Pereirinha <marco.pereirinha@xwp.co>
2023-01-17 12:17:17 +0000

    Move image crop size feature under the `cloudinary_enabled_crop_sizes` filter

--- ui-definitions/settings-metaboxes.php
+++ ui-definitions/settings-metaboxes.php
@@ -5,21 +5,42 @@
  * @package Cloudinary
  */
 
-$metaboxes = array(
-	'crop_meta' => array(
-		'title'    => __( 'Cloudinary crop sizes', 'cloudinary' ),
-		'screen'   => 'attachment',
-		'settings' => array(
-			array(
-				'slug' => 'single_crop_sizes',
-				'type' => 'stand_alone',
+use Cloudinary\Utils;
+
+$metaboxes = array();
+
+/**
+ * Enable the crop size settings.
+ *
+ * @hook  cloudinary_enabled_crop_sizes
+ * @since 3.1.0
+ * @default {false}
+ *
+ * @param $enabeld {bool} Are the crop sizes enabled?
+ *
+ * @retrun {bool}
+ */
+$enabled_crop_sizes = apply_filters( 'cloudinary_enabled_crop_sizes', false );
+
+if ( $enabled_crop_sizes ) {
+	$metaboxes = array(
+		'crop_meta' => array(
+			'title'    => __( 'Cloudinary crop sizes', 'cloudinary' ),
+			'screen'   => 'attachment',
+			'settings' => array(
 				array(
-					'type'         => 'on_off',
-					'slug'         => 'enable_single_sizes',
-					'title'        => __( 'Sized transformations', 'cloudinary' ),
-					'tooltip_text' => __(
-						'Enable transformations per registered image sizes.',
-						'cloudinary'
+					'slug' => 'single_crop_sizes',
+					'type' => 'stand_alone',
+					array(
+						'type'         => 'on_off',
+						'slug'         => 'enable_single_sizes',
+						'title'        => __( 'Crop sizes (beta)', 'cloudinary' ),
+						'tooltip_text' => __(
+							'Enable transformations per registered image sizes.',
+							'cloudinary'
+						),
+						'description'  => __( 'Enable crop sizes.', 'cloudinary' ),
+						'default'      => 'off',
 					),
 					array(
 						'type'      => 'sizes',
@@ -38,8 +59,8 @@ $metaboxes = array(
 				),
 			),
 		),
-	),
-);
+	);
+}
 
 /**
  * Filter the meta boxes.
