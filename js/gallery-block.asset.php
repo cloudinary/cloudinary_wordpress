7fb99404d200d1195e6efa539aaa84e8e926e683
Marco Pereirinha <marco.pereirinha@xwp.co>
2023-08-18 12:54:38 +0100

    Update the static assets

--- js/gallery-block.asset.php
+++ js/gallery-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd23010e61311abad3155');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '28feafff607fad7cbf51');
