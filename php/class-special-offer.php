3c6e13bc5720d3bbe07a5069bb6a78e58ef85f14
Marco Pereirinha <marco.pereirinha@xwp.co>
2024-08-28 13:24:56 +0100

    Update wording

--- php/class-special-offer.php
+++ php/class-special-offer.php
@@ -44,7 +44,7 @@ class Special_Offer {
 	 * @return array
 	 */
 	public function filtered_settings( $settings ) {
-		if ( ! $this->is_29_offer_available() ) {
+		if ( ! $this->is_special_offer_available() ) {
 			return $settings;
 		}
 
@@ -52,7 +52,7 @@ class Special_Offer {
 			array(
 				'type'       => 'tag',
 				'element'    => 'div',
-				'content'    => __( 'Special offer', 'cloudinary' ),
+				'content'    => __( 'Special Offer', 'cloudinary' ),
 				'attributes' => array(
 					'class' => array(
 						'cld-special-offer',
@@ -61,17 +61,17 @@ class Special_Offer {
 			),
 			array(
 				'type'        => 'panel',
-				'title'       => __( 'Get a $29 plan', 'cloudinary' ),
+				'title'       => __( 'Get a small $29 plan', 'cloudinary' ),
 				'description' => __( 'Contact us', 'cloudinary' ),
 				'collapsible' => 'closed',
 				array(
 					'type'    => 'tag',
-					'element' => 'p',
-					'content' => __( 'Get 100GB for $29.', 'cloudinary' ),
+					'element' => 'div',
+					'content' => $this->get_special_offer_content(),
 				),
 				array(
 					'type'    => 'link',
-					'content' => __( 'Lets get it started', 'cloudinary' ),
+					'content' => __( 'Get started', 'cloudinary' ),
 					'url'     => static function () {
 						$current_user = wp_get_current_user();
 						$plugin       = get_plugin_instance();
@@ -83,8 +83,8 @@ class Special_Offer {
 								'tf_22246877'                  => $current_user->display_name,
 								'tf_360007219560'              => $cloud_name,
 								'tf_360017815680'              => 'help_with_plans',
-								'tf_subject'                   => __( 'Special offer for $29 plan', 'cloudinary' ),
-								'tf_description'               => __( 'I would like to get the $29 plan', 'cloudinary' ),
+								'tf_subject'                   => __( 'Request to Purchase the Small Plan', 'cloudinary' ),
+								'tf_description'               => __( "Hello,\n\nI'm interested in purchasing the Small plan for $29. Could you please provide me with the next steps to complete the purchase?\n\nThank you!", 'cloudinary' ),
 							),
 							'https://support.cloudinary.com/hc/en-us/requests/new'
 						);
@@ -102,9 +102,21 @@ class Special_Offer {
 	 *
 	 * @return bool
 	 */
-	protected function is_29_offer_available() {
+	protected function is_special_offer_available() {
 		$last_usage = get_option( Connect::META_KEYS['last_usage'], array( 'plan' => '' ) );
 
 		return 'free' !== strtolower( $last_usage['plan'] );
+	}
+
+	/**
+	 * Get Special Offer content.
+	 *
+	 * @return string
+	 */
+	protected function get_special_offer_content() {
+		ob_start();
+		include $this->plugin->dir_path . 'php/templates/special-offer.php'; // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable
+
+		return ob_get_clean();
 	}
 }
