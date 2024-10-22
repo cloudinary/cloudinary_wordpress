6507c519f765ffc8ee25b3fd0fe1a6f0e7a70b8b
Marco Pereirinha <marco.pereirinha@xwp.co>
2024-10-22 15:36:17 +0100

    Wording update

--- php/templates/taxonomy-transformation-fields.php
+++ php/templates/taxonomy-transformation-fields.php
@@ -20,19 +20,23 @@ $cloudinary = get_plugin_instance();
 <div class="cloudinary-collapsible">
 	<div class="cloudinary-collapsible__toggle">
 		<h2>
-			<?php esc_html_e( 'Cloudinary transformations', 'cloudinary' ); ?>
+			<?php
+			// translators: The taxonomy label.
+			echo esc_html( sprintf( __( 'Cloudinary %s transformations', 'cloudinary' ), $tax_labels->singular_name ) );
+			?>
 		</h2>
 		<button type="button"><i class="dashicons dashicons-arrow-down-alt2"></i></button>
 	</div>
 	<div class="cloudinary-collapsible__content" style="display:none;">
 		<div class="cld-more-details">
 			<?php
-			echo esc_html(
-				sprintf(
-					// translators: The taxonomy label.
-					__( 'Additional transformations for this %s that will be appended to the globally defined Cloudinary transformations.', 'cloudinary' ),
-					$tax_labels->singular_name
-				)
+			printf(
+				// translators: %1$s is the taxonomy label, %2$s is the image settings link, %4$s is the video settings link. The %3$s is the closing tags for the links.
+				esc_html__( 'Add these %1$s-specific transformations to the global Cloudinary transformations in the plugin\'s %2$simage%3$s and %4$svideo%3$s settings.', 'cloudinary' ),
+				esc_html( $tax_labels->singular_name ),
+				'<a href="' . esc_url( admin_url( 'admin.php?page=cloudinary_image_settings#text-image-settings.image-freeform' ) ) . '">',
+				'</a>',
+				'<a href="' . esc_url( admin_url( 'admin.php?page=cloudinary_video_settings#text-video-settings.video-freeform' ) ) . '">'
 			)
 			?>
 		</div>
