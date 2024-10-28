3d35f00f7a7177d6f52cf899572584bbf3eaa222
Marco Pereirinha <marco.pereirinha@xwp.co>
2024-10-28 17:21:53 +0000

    Update compiled assets

--- js/block-editor.asset.php
+++ js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '87ba7d528e0425bc85ca');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '37e2404e0016dbc6780b');
