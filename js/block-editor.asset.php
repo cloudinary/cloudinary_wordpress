ead10ab5865db6353c5453a68ac13c2435c14508
Jaroslav Polakovič <j.polakovic@gmail.com>
2025-02-23 17:44:30 +0100

    Don't initialize terms inspector if the wrapper is not in page.

--- js/block-editor.asset.php
+++ js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'c8e1eb2b75a7df74cf6a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9b0123a912a0a6182e6b');
