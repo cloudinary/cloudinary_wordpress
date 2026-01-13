5cf7742f4d051f7e50cc390a13e2ad2df8327280
Aleksandar Atanasov <aatanasovdev@gmail.com>
2026-01-13 16:57:35 +0200

    Change effect sizes to percentages

--- js/deactivate.asset.php
+++ js/deactivate.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '4d933d9f3b17a0fbc78f');
+<?php return array('dependencies' => array(), 'version' => '7696f646fa350b4c84b0');
--- js/gallery-block.asset.php
+++ js/gallery-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1f5b975cfd156d6dfc62');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ae052717d7e9a2152053');
