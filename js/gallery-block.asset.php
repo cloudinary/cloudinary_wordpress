67e9a3d5a5ab3a2deacdd67a969ba5ad89f307cf
Gabriel de Tassigny <gabriel.detassigny@xwp.co>
2026-04-07 10:48:02 +0200

    Upgrade gallery block to apiVersion 2

--- js/gallery-block.asset.php
+++ js/gallery-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8cc16b66e5e52f290d81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fee80877520a08e9e0b4');
