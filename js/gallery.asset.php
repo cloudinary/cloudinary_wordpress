2570c30a1bf0b7ac09c24d76dcea95c002681304
Gabriel de Tassigny <gabriel.detassigny@xwp.co>
2026-04-13 15:16:12 +0200

    Switch JSX transform from automatic to classic runtime

--- js/gallery.asset.php
+++ js/gallery.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0af55e1154e80535a210');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ce273b9408344c97f28b');
