88f76907cd5a363f94ce774dcd5b364c87b09d09
Gabriel de Tassigny <gabriel.detassigny@xwp.co>
2026-09-22 11:00:38 +0200

    Update build assets

--- js/block-editor.asset.php
+++ js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '58afb0a9ee7fa6faecd7');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'a9b2df98cf4781d2997f');
--- js/gallery.asset.php
+++ js/gallery.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e09fae43ef7020fbad7b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dd272475a4c14b284cf4');
